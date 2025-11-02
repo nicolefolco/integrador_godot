e115eab43dc19b70b41f34fd0b3fbe218ec1ef57
IgnacioMojsa <ignaciomojsa02@gmail.com>
2025-11-02 19:16:25 -0300

    Problema con animacion en bucle solucionado

--- escena_principal/animated_sprite_2d.gd
+++ escena_principal/animated_sprite_2d.gd
@@ -1,10 +1,9 @@
 extends AnimatedSprite2D
 
 var estado_actual = "idle"
-var boton_apretado = false
 var contador_clicks_golpear = 0 
 
-func _ready() -> void:
+func _ready():
 	play("idle")
 
 func estado_personaje(unEstado):
@@ -19,20 +18,20 @@ func animacion_recibe_danio():
 	estado_actual = "recibe_danio"
 
 func animacion_recibe_danio_2():
-	play("recibe_danio_2")
-	estado_actual = "recibe_danio_2"
+	if (estado_actual == "idle" and contador_clicks_golpear >= 3 and contador_clicks_golpear <= 5):
+		play("recibe_danio_2")
+		estado_actual = "recibe_danio_2"
+		contador_clicks_golpear += 1
+	
 
-func _on_golpear_pressed() -> void:
-	boton_apretado = true
+func _on_golpear_pressed():
 	if (estado_actual == "idle" and contador_clicks_golpear >= 0 and contador_clicks_golpear <= 1):
 		animacion_recibe_danio()
 		contador_clicks_golpear +=1
-	elif (estado_actual == "idle" and contador_clicks_golpear >= 3 and contador_clicks_golpear <= 5):
+	else:
 		animacion_recibe_danio_2()
-		contador_clicks_golpear += 1
 
 
-func _on_animation_finished() -> void:
+func _on_animation_finished():
 	if estado_actual == "recibe_danio" or "recibe_danio_2":
-		boton_apretado = false
 		animacion_idle()

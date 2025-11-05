3d23aa2653698ac985bf825dfce4501f1de50a84
nicolefolco <folconicole1@gmail.com>
2025-11-05 20:09:56 -0300

    delta con libelulas

--- escena_principal/animated_sprite_2d.gd
+++ escena_principal/animated_sprite_2d.gd
@@ -6,9 +6,6 @@ var contador_clicks_golpear = 0
 func _ready():
 	play("idle")
 
-func estado_personaje(unEstado):
-	estado_actual = unEstado
-
 func animacion_idle():
 	play("idle")
 	estado_actual = "idle"
@@ -18,20 +15,29 @@ func animacion_recibe_danio():
 	estado_actual = "recibe_danio"
 
 func animacion_recibe_danio_2():
-	if (estado_actual == "idle" and contador_clicks_golpear >= 3 and contador_clicks_golpear <= 5):
 		play("recibe_danio_2")
 		estado_actual = "recibe_danio_2"
-		contador_clicks_golpear += 1
-	
+
+func animacion_se_enoja_mucho():
+	play("se_enoja_mucho")
+	estado_actual = "se_enoja_mucho"
 
 func _on_golpear_pressed():
 	if (estado_actual == "idle" and contador_clicks_golpear >= 0 and contador_clicks_golpear <= 1):
 		animacion_recibe_danio()
 		contador_clicks_golpear +=1
-	else:
+	elif (estado_actual == "idle" and contador_clicks_golpear >= 2 and contador_clicks_golpear <= 5 ):
 		animacion_recibe_danio_2()
+		contador_clicks_golpear += 1
+	else:
+		animacion_se_enoja_mucho()
 
 
 func _on_animation_finished():
 	if estado_actual == "recibe_danio" or "recibe_danio_2":
 		animacion_idle()
+
+func _on_reiniciar_pressed() -> void:
+	stop()
+	animacion_idle()
+	contador_clicks_golpear = 0
--- escena_principal/escena_principal.gd
+++ escena_principal/escena_principal.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+const libelulas = preload("res://libelulas/libelulas.tscn")
+
+func _on_timer_timeout() -> void:
+	var libelula = libelulas.instantiate()
+	var x = randf_range(-50, -10)
+	var y = randf_range(0, 400)
+	libelula.position = Vector2(x, y)
+	add_child(libelula)
